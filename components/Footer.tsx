5112ef9fd7feb36af77fb6aa1c05da1516141513
Hugo Martinez <contact@hmartinez.fr>
2024-09-05 00:08:36 +0200

    Add connection to db

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,50 @@
+import { sql } from "@vercel/postgres";
+
 interface SignatureProps {
-  name: string;
-  title: string;
+  id: number;
+  prenom: string;
+  nom: string;
+  ville: string;
+  code_postal: string;
+  fonction: string;
 }
 
-const Signature: React.FC<SignatureProps> = ({ name, title }) => (
+const Signature: React.FC<SignatureProps> = ({
+  id,
+  prenom,
+  nom,
+  ville,
+  code_postal,
+  fonction,
+}) => (
   <div>
-    {name} <span className="">, {title}</span>
+    {prenom} {nom}
+    {", "}
+    <br />
+    <span className="font-normal">
+      {fonction} de {ville} ({code_postal})
+    </span>
   </div>
 );
 
-export default function Footer() {
-  const signatures: SignatureProps[] = [
-    { name: "Hugo Martinez", title: "Président de l'Association HUGO!" },
-    { name: "Hugo Martinez", title: "Président de l'Association HUGO!" },
-    { name: "Hugo Martinez", title: "Président de l'Association HUGO!" },
-  ];
+export default async function Footer() {
+  const { rows, fields } =
+    await sql`SELECT id, prenom, nom, ville, code_postal, fonction FROM signatures`;
+
+  console.log(rows);
 
   return (
     <footer className="flex flex-wrap gap-5 justify-between mt-14 max-w-full text-2xl font-bold leading-7 text-black w-[948px] max-md:mt-10">
-      {signatures.map((sig, index) => (
-        <Signature key={index} name={sig.name} title={sig.title} />
+      {rows.map((row, index) => (
+        <Signature
+          key={row.id}
+          nom={row.nom}
+          fonction={row.fonction}
+          id={row.id}
+          code_postal={row.code_postal}
+          prenom={row.prenom}
+          ville={row.ville}
+        />
       ))}
     </footer>
   );
